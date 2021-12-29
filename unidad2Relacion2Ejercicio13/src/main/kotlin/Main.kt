23016f5ba2aae0a237528d62bd84ff0b634b27e6
Daniel Rodríguez Fernández <daniel.rodriguezfernandez@alumno.iesluisvives.org>
2021-12-30 00:31:56 +0100

    actualizaciones día 29/12/2021

---
+++ unidad2Relacion2Ejercicio13/src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    println("Calcular cociente y resto mediante restas sucesivas")
+    println("Introduce el número que deseas dividir: ")
+    var resultado: Int = 0
+    var cociente: Int = readLine()?.toInt() as Int
+    println("¿Entre cuanto quieres dividir? ")
+    var dividendo: Int = readLine()?.toInt() as Int
+    var resto: Int = 0
+    
+    //Desarrollo del programa
+
+
+}
