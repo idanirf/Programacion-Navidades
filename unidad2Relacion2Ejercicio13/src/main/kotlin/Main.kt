9271577dfeb8e1a3fe85cd556d2141fd93caa136
Daniel Rodríguez Fernández <daniel.rodriguezfernandez@alumno.iesluisvives.org>
2021-12-30 01:45:53 +0100

    actualización 30/12/2021

--- unidad2Relacion2Ejercicio13/src/main/kotlin/Main.kt
+++ unidad2Relacion2Ejercicio13/src/main/kotlin/Main.kt
@@ -5,9 +5,14 @@ fun main(args: Array<String>) {
     var cociente: Int = readLine()?.toInt() as Int
     println("¿Entre cuanto quieres dividir? ")
     var dividendo: Int = readLine()?.toInt() as Int
-    var resto: Int = 0
-    
-    //Desarrollo del programa
+    var resto: Int = cociente
 
+    //Desarrollo del programa
+    while(resto>=dividendo){
+        resto = (resto - dividendo)
+        resultado = (resultado + 1)
+    }
+    println("El resultado de la división es: $resultado")
+    println("El resto de la división es: $resto")
 
 }
