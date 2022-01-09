c771ab66d1ceb21d341517847c0482b2e0adf92d
Daniel Rodríguez Fernández <daniel.rodriguezfernandez@alumno.iesluisvives.org>
2022-01-09 03:05:47 +0100

    actualizacion 08/01/2021

--- unidad3Relacion1Ejercicio1/src/main/kotlin/ordenacionQuicksort.kt
+++ unidad3Relacion1Ejercicio1/src/main/kotlin/ordenacionQuicksort.kt
@@ -1,29 +1,44 @@
 fun main(args: Array<String>) {
-    println("Imprimir vector")
     val vectorEnteros = intArrayOf(2, 4, 89, 23, 12)
-    val ordenadoQuicksort = quickSort(vectorEnteros, 0, vectorEnteros.size - 1)
-    print("El vector ordenado por QUICKSORT es: $ordenadoQuicksort")
+    quickSort(vectorEnteros, 0, vectorEnteros.size-1)
+    print("El vector ordenado por QUICKSORT es: ")
+    for(i in vectorEnteros){
+        print(" ")
+        print(i)
+    }
+  }
+
+fun quickSort(vectorEnteros: IntArray, posicionIzquierda: Int, posicionDerecha: Int) {
+    val i = particion (vectorEnteros, posicionIzquierda, posicionDerecha)
+    if(posicionIzquierda < i-1) {
+        quickSort(vectorEnteros, posicionIzquierda, i-1)
+    }
+    if(i < posicionDerecha) {
+        quickSort(vectorEnteros,i, posicionDerecha)
+    }
 }
 
-fun quickSort(vectorEnteros: IntArray, posicionIzquierda: Int, posicionDerecha: Int): IntArray? {
-    val pivote = vectorEnteros[posicionIzquierda]
-    var i = posicionIzquierda
-    var j = posicionDerecha
-    var aux: Int
-    while (i < j) {
-        while (vectorEnteros[i] <= pivote && i < j)
-            i++
-        while (vectorEnteros[j] > pivote)
-            j--
-        if (i < j) {
-            aux = vectorEnteros[i]
-            vectorEnteros[i] = vectorEnteros[j]
-            vectorEnteros[j] = aux
+fun particion(vectorEnteros: IntArray, posicionIzquierda: Int, posicionDerecha: Int): Int {
+    var izquierda = posicionIzquierda
+    var derecha = posicionDerecha
+    val pivote = vectorEnteros[(izquierda + derecha)/2]
+    while (izquierda <= derecha) {
+        while (vectorEnteros[izquierda] < pivote)
+            izquierda++
+        while (vectorEnteros[derecha] > pivote)
+            derecha--
+
+        if (izquierda <= derecha) {
+            vectorIntercambio(vectorEnteros, izquierda,derecha)
+            izquierda++
+            derecha--
         }
     }
-    vectorEnteros[posicionIzquierda] = vectorEnteros[j]
-    vectorEnteros[j] = pivote
-    if (posicionIzquierda < j - 1) quickSort(vectorEnteros, posicionIzquierda, j - 1)
-    if (j + 1 < posicionDerecha) quickSort(vectorEnteros, j + 1, posicionDerecha)
-    return vectorEnteros
+    return izquierda
+}
+
+fun vectorIntercambio(a: IntArray, b: Int, c: Int) {
+    val temporal = a[b]
+    a[b] = a[c]
+    a[c] = temporal
 }
